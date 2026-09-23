1fc69e95fd73c4de46dc5edc8f3b3d071a772b15
powerplayexe <nivedsreejaharidasan@gmail.com>
2026-09-23 21:03:54 +0530

    Avoid duplicate Ask JARVIS installation log

--- main.py
+++ main.py
@@ -1115,8 +1115,7 @@ class JarvisLive:
             "add ask jarvis context menu",
             "add ask jarvis to right click menu",
         ):
-            result = self._run_local_action("ask_jarvis_context", {"action": "install_menu"})
-            self.ui.write_log("SYS: " + str(result))
+            self._run_local_action("ask_jarvis_context", {"action": "install_menu"})
             return True
 
         # Emergency stop controls are always local and remain available
